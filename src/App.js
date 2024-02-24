99b9f565a374e38626855f008f227e86aae7a935
pjha7 <prashant_jha1@intuit.com>
2024-02-24 16:42:46 +0530

    comment added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   const ToDoContext = lazy(()=>import('./ToDoReducer'));
   return(
     <Suspense fallback={<>loading...</>} >
+      {/* You can render <ToDo/> and check it's functionality to know the difference
+      between a simple app and app created using useReducer hook.  */}
     <ToDoContext/>
     </Suspense>
   )
